6a15988e18f624d3d5aa9807368d95e51dcdb421
Aldair Dorantes Martínez <Aldair25dorantes@gmail.com>
2022-10-12 17:57:43 -0500

    Uso de variable int y Apunte int/String

--- Curso.java
+++ Curso.java
@@ -1,6 +1,13 @@
 public class Curso {
     public static void main(String args[]){
-        System.out.println("Hola que tal");
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //I done an int variable type, then I printed it;
+        //Hice una variable de tipo entero y la imprimí;
+miVariableEntera= 20;
+        System.out.println(miVariableEntera);
+        //I changed 20 instead of 10 then I printed it;
+        //Cambie 10 por 20 y lo imprimí;
 
     }
 }
