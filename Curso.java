ad68f5ac01544e8b943bb0bb0319551db21acaae
Aldair Dorantes Martínez <Aldair25dorantes@gmail.com>
2022-10-14 10:38:15 -0500

    Concatenación, contexto cadena, paréntesis y correción ortográfica

--- Curso.java
+++ Curso.java
@@ -1,34 +1,71 @@
 public class Curso {
     public static void main(String args[]){
-        //Ahora trabajaré con una variable de tipo String.
-        //Now i will use a String Type variable.
-        String miVariableCadena = "Saludos";
-        System.out.println(miVariableCadena);
-        //Tarea: Darle un nuevo valor a la variable y diga adios.
-        //HW: Change the String variable value into Goodbye.
-        miVariableCadena = "Adios";
-        System.out.println(miVariableCadena);
-        //Éxito.
-        //Success.
-
-        /**Aqui haré un punto y aparte respecto al código anterior
-         *
+        /**
+         * CONCATENACIÓN DE CADENAS
          */
-//Variable de tipo var se refiere a que se dará por hecho el tipo se variable que
-        //estemos usando según el valor que le demos, ya sea String, float o int.
+        //Usando var con inferencia de tipos vamos a definir 2 variables las cuales
+        //vamos a concatenar
+        var miVariable1 = "Hola Usuario numero";
+        var miVariable2 = 25;
+        System.out.println(miVariable1 + miVariable2);
+        //Debajo voy a definir una nueva variable la cual automáticamente va a
+        //sumar ambas variables y de esa forma solo imprimiré una sola variable
+        var miVariableUnion = miVariable1+miVariable2;
+        System.out.println(miVariableUnion);
+        //Ahora imprimiré la variable unión con su valor
+        System.out.println("miVariableUnion = " + miVariableUnion);
+        //Ahora meteré un comando nuevo para crear espacio" "entre la concatenación
 
-        /**Definimos la variable
-         *
+        miVariableUnion= miVariable1 + " " + miVariable2;
+        System.out.println(miVariableUnion);
+//Aquí volví a asignar valor a la Variable union con el cambio de las comillas para
+        //asi generar un espacio y al final la imprimí
+        //De esta forma ya concatenamos una variable entera y una variable string
+        /**
+         * Operaciones de variables de tipo entero y concatenación
          */
-        var miVariableVar = 15;
-        System.out.println(miVariableVar);
-        //Tarea escribir mi variable var 2 en forma de string e imprimirlo
-        var miVariableVar2 = "Soy una variable String reconocida mediante var";
-        System.out.println(miVariableVar2);
-        System.out.println("miVariableVar2 = " + miVariableVar2);
-        //En esa última variable lo que hicimos fue usar el comando soutv
-        //el cual va a generar ese atajo en el que escribirá el nombre de la variable
-        //y el valor de la misma y la inferencia es a partir de la versión 9 de jdk
-        //en adelante.
+       var A = 5;
+       var B = 6;
+        System.out.println(A+B);
+        //En la línea de arriba concatené mediante signo más ambas variables
+        //lo cual da como resultado la suma de sus valores y ahora lo haré con
+        //los otros símbolos de operación * / -.
+        System.out.println(A-B);
+        //Funcionó
+        System.out.println(A*B);
+        System.out.println(A/B);
+        //En este punto me dio 0 por lo cual voy a aumentar el valor de alguna
+        //variable "A" con el fin de que tenga más cantidad sobre la cual se va a
+        //dividir.
+        A = 30;
+        System.out.println(A/B);
+        //Efectivamente, podemos aumentar el numero y que de esta forma no nos de
+        //0.
+        System.out.println(A+B+miVariableUnion);
+        //En esta variable lo que hice fue concatenar 2 variables enteras que se
+        //van a sumar y luego se les va a sumar la variable unión que consta de
+        //una previa concatenación de variable entera con string entonces eso se
+        //verá impresa con una suma de la variable A y B y se va a concatenar con unión
+        //Corrió perfectamente.
+        /**
+         * CONTEXTO CADENA
+         */
+        //Ahora empezaré de izquierda a derecha, pero iniciaré con una cadena
+        //para que actúe la regla contexto cadena de izquierda a derecha y únicamente
+        //se impriman los números sin interactuar de manera operacional
+        System.out.println(miVariable1 + A + B);
+        //Éxito
+        /**
+         * EVITAR CONTEXTO CADENA
+         */
+        //Evitaré el contexto cadena escribiendo las enteras dentro un paréntesis
+        //extra dentro del print
+        System.out.println(miVariable1+(A+B));
+        //Funciona.
+        //Significa que los paréntesis cambian la prioridad de lo que se va a generar.
+        //FinClase
+
+
+        
     }
 }
