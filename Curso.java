1439d7e0e4ef585f1373e55c0e645872fd5cd615
Aldair Dorantes Martínez <Aldair25dorantes@gmail.com>
2022-11-08 19:38:37 -0600

    Mini Proyecto Scanner Fin 1 y 2.

--- Curso.java
+++ Curso.java
@@ -1,107 +1,76 @@
-import java.util.Scanner;
+import java.util.Scanner; //Uso que se añadió gracias al paso de java.util.
 
 public class Curso {
+
     public static void main(String args[]) {
-          /*
-          CLASE DE REPASO TIPOS DE VARIABLES, CONCATENACIONES, SCANNER ETC...
-           */
-        //CONCATENACIÓN
-        //Definimos las variables
-        String miVariableString = "Soy una inferencia de tipo cadena o string";
-        int miVariableInt = 10;
-        var miVariableVar = "Soy una inferencia de tipo var de texto";
-        var miVariableVar2 = 20;
 
-        System.out.println(miVariableString + " " +miVariableInt + " " + miVariableVar + " " + miVariableVar2);
+        /**
+        EJERCICIO SCANNER Y DOCUMENTACIÓN.
+        (TODOS LOS SALTOS ("\n") SON ESTÉTICOS).
+         **/
 
-        //Propiedad de la cadena
-        System.out.println(miVariableString+ " " + miVariableInt+miVariableVar2);
-        //Funcionó correctamente
+        Scanner lector = new Scanner(System.in);
+        /*Declaro una variable de tipo "Scanner" la cual tiene el nombre de "lector" y utilizo la estructura "new
+        Scanner(System.in);" la cual me da acceso a la consola de escritura mediante Scanner, después de esto me
+        sitúo con el cursor encima de la palabra "Scanner" y saldrá una pequeña ventana donde nos va a facilitar
+        la opción java.util la cual asigno y de esta forma ya está activa la función Scanner.
+         */
 
-        //Rompamos la propiedad de la cadena
-        System.out.println(miVariableString+(miVariableInt+miVariableVar2));
+        System.out.println("\n");
+        //Salto estético de línea.
+
+        System.out.println("Bienvenido a HSBC\n");
+        System.out.println("Para crear una cuente ingrese los siguientes datos.\n");
+        System.out.println("Nombre(s):");
+        //Primero declaro el feed de la empresa junto con las instrucciones y la sección de datos.
+
+        var nombresUsuario = lector.nextLine(); //(PASO 1.0).
+        /*Declaro una variable con un nombre relacionado el dato pedido y la igualo a la variable "lector" y añado
+        el termino ".nextLine();" el cual hace que el programa no avance hasta que escribamos la información que se
+        nos pide. (POR PRACTICIDAD ESTE PASO SE LLAMARÁ "1.0").
+         */
+                                                 //----//
         /*
-        Con el uso del doble paréntesis si se va a efectuar la operación, ya que
-        es una regla similar al álgebra donde si lo hacen dentro de los ya mencionados
+        REPETÍ EL PROCESO ANTERIOR PERO SIN DECLARAR OTRA VARIABLE SCANNER, ÚNICAMENTE SEGUÍ LOS MISMOS PASOS
+        QUE EN EL PASO "1.0".
          */
 
-        //Suma de enteros
-        System.out.println(miVariableInt+miVariableVar2);
-        //Funcionó
+        System.out.println("Apellido paterno:");
+        var apellidoUno = lector.nextLine();
 
-        //Soutv imprime lo siguiente:
-        System.out.println("miVariableVar2 = " + miVariableVar2);
-        
-        //Creación de una variable hibrida con el valor de otras 2
-        var miVariableHibrida = miVariableInt+miVariableVar2;
-        //Imprimimos con soutv
-        System.out.println("miVariableHibrida = " + miVariableHibrida);
-        //nice
+        System.out.println("Apellido materno:");
+        var apellidoDos = lector.nextLine();
 
-        //Cambio de valor en variables
-        miVariableHibrida = 50;
-        //Imprimimos
-        System.out.println("miVariableHibrida = " + miVariableHibrida);
-        //nice
+        System.out.println("Correo electrónico:");
+        var correoEmail = lector.nextLine();
 
-        /*Caracteres especiales en java Usando ctrl alt más diagonal inversa la obtendremos
-        "\".
+        System.out.println("Número telefónico:");
+        var numeroDeTel = lector.nextLine();
+
+        System.out.println("Número de tarjeta a 16 dígitos:");
+        var numeroDeTarjeta = lector.nextLine();
+
+        var nombreCompleto = nombresUsuario + " " + apellidoUno + " " + apellidoDos;
+        //Hago una variable que contenga todos los datos leídos previamente sobre los nombres para imprimir todo junto.
+
+        System.out.println("\n");
+
+        System.out.println("Bienvenido(a) a HSBC" + " " + "\"" + nombreCompleto + "\"" + " " + "con dirección" + " " +
+                "de correo electrónico" +" "+ "\"" + correoEmail + "\"" + "\n" + "te hemos enviado una clave de" +" "+
+                "activación vía correo electrónico, la cual tiene vigencia de 5 minutos, te esperamos!.\n");
+        /*Hago un mensaje donde confirmo que he recibido todos los datos y con ello mandar una confirmación de cuenta
+        mediante el correo electrónico proporcionado por el usuario.
+        (Hago uso de "+" para concatenar, " " para hacer espacio entre palabras y "\n" para saltos de linea).
          */
 
-        //Carácter \n sirve para hacer un salto de línea dentro de una impresión de texto
-        var miVariableBrinco = "Brinco";
-        System.out.println("miVariableBrinco = \n" + miVariableBrinco + "\n"+ miVariableString);
-        //Como vemos, en lugar de imprimirse enfrente lo que se hace es un salto de línea
+        System.out.println("Número de tarjeta:" + " "+ numeroDeTarjeta + "." + "\n");
+        //Imprimo la confirmación del número de tarjeta dado.
 
-        //Carácter \t, \b, \' y \"
-        var A = "Aldair";
-        var B = 20;
-        var C = "Años";
-        var D = 2002;
-        //Haré un pequeño print con todas
-        System.out.println("Nombre:\n" + "\"" + A + "\"" + "\n" + "Edad: \b" + "\"" + B + " " + C +
-                "\"" + "\n" + "Año de nacimiento:'" + D + "'" + "\t" + "Bienvenido de nuevo.");
-        //Funciona perfecto
+        System.out.println("\n");
+        System.out.println("HSBC " + "\"" + "Todos los derechos reservados SA de CV 2022." + "\"");
+        //Salto de línea estético y copyright de la empresa.
 
-
-        /* SCANNER
-        Visto lo anterior podemos darle el nombre de código duro o estático
-        eso significa que nosotros le estamos dando un valor, pero no es algo
-        que pueda cambiar dependiendo el tiempo o persona, scanner se encarga.
-        Lo definiremos como una variable de tipo scanner con su nombre y un igual
-        más el valor new Scanner(System.in), una vez acabemos eso nos posicionamos en
-        el foco o encima de la palabra scanner y le damos en java.util o import ahí
-        para que funcione.
-         */
-
-        Scanner consola = new Scanner(System.in);
-        //Definimos la variable consola de tipo scanner.
-
-        /*Ahora vamos a escribir la indicación a seguir y
-        definir una variable que se pueda leer sobre sí misma
-        varias veces con diferente información y también bajo otros nombres y
-        lo igualaremos al nombre de la variable scanner junto con la propiedad
-        .nextLine(); lo cual nos dará el poder para leer muchas cosas.
-         */
-        System.out.println("Escribe tu nombre:");
-        var usuario = consola.nextLine();
-        //Debemos escribir info y solo ahí se detendrá el programa
-        System.out.println("Bienvenido" + " " + usuario);
-
-        /*metemos otra variable para leer únicamente siguiendo la instrucción
-        de poner primero la instrucción luego declarar la variable.
-         */
-
-        System.out.println("Escribe tu numero de cuenta:");
-        var numeroCuenta = consola.nextLine();
-        System.out.println("No.de cuenta:" + numeroCuenta);
-
-
-        //Tarea: Hacer un ejercicio propio de Scanner.
-
-
-
-
+        //FIN.
     }
 }
 
